da09416b374d7795a7ece0889b78944ce4d95a3f
doi <porebskid8@gmail,com>
2025-03-27 20:12:20 +0100

    Setup initial .NET backend with SignalR, EntityFramework and SQL Server

---
+++ backend/Checkers.API/Hubs/GameHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace Checkers.API.Hubs
+{
+    public class GameHub : Hub
+    {
+        public async Task SendMove(string moveData)
+        {
+            await Clients.Others.SendAsync("ReceiveMove", moveData);
+        }
+    }
+}
